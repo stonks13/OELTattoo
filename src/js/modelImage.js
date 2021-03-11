c685b009014cfc0fa03d76974602fe07d3b63602
stonks13 <escarvatopo1@gmail.com>
2021-03-11 08:56:29 +0100

    Todo

---
+++ src/js/modelImage.js
@@ -0,0 +1,19 @@
+var modal = document.getElementById("myModal");
+function zoomImage(srcUrlImage){
+	var img = document.getElementById(srcUrlImage);
+	var modalImg = document.getElementById("img01");
+	var captionText = document.getElementById("caption");
+	img.onclick = function(){
+		modal.style.display = "block";
+		modalImg.src = this.src;
+		captionText.innerHTML = this.alt;
+	}
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() { 
+		modal.style.display = "none";
+	}
+}
---
+++ src/js/navfunctions.js
@@ -0,0 +1,6 @@
+function openFunction(){
+	document.getElementById("menu").style.width="250px";
+}
+function closeFunction(){
+	document.getElementById("menu").style.width="0px";
+}
